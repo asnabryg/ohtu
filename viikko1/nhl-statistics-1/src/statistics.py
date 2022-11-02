8743e8828c414894ffd7fe1366d95a05c7796699
asnabryg <8mixu8@gmail.com>
2022-11-02 15:19:54 +0200

    17

--- viikko1/nhl-statistics-1/src/statistics.py
+++ viikko1/nhl-statistics-1/src/statistics.py
@@ -1,7 +1,22 @@
+from enum import Enum
+
+
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 
 def sort_by_points(player):
     return player.points
+
+
+def sort_by_goals(player):
+    return player.goals
+
+
+def sort_by_assists(player):
+    return player.assists
 
 
 class Statistics:
@@ -25,11 +40,19 @@ class Statistics:
 
         return list(players_of_team)
 
-    def top(self, how_many):
+    def top(self, how_many, sortBy=SortBy.POINTS):
+
+        def getSort(sortBy):
+            if sortBy == SortBy.POINTS:
+                return sort_by_points
+            if sortBy == SortBy.GOALS:
+                return sort_by_goals
+            return sort_by_assists
+
         sorted_players = sorted(
             self._players,
             reverse=True,
-            key=sort_by_points
+            key=getSort(sortBy)
         )
 
         result = []
--- viikko1/nhl-statistics-1/src/tests/statistics_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -1,5 +1,6 @@
 import unittest
 from statistics import Statistics
+from statistics import SortBy
 from player import Player
 
 
@@ -25,22 +26,45 @@ class TestStatistics(unittest.TestCase):
         self.assertEqual(self.stats._players[2].name, "Kurri")
         self.assertEqual(self.stats._players[3].name, "Yzerman")
         self.assertEqual(self.stats._players[4].name, "Gretzky")
-    
+
     def test_search_returns_correct_player(self):
         self.assertEqual(self.stats.search("Kurri").name, "Kurri")
-    
+
     def test_search_returns_none(self):
         self.assertEqual(self.stats.search("Selänne"), None)
-    
+
     def test_get_players_of_team(self):
         players = self.stats.team("EDM")
         for player in players:
             self.assertEqual(player.team, "EDM")
 
-    def test_top(self):
+    def test_top_points(self):
         players = self.stats.top(4)
         self.assertEqual(players[0].name, "Gretzky")
         self.assertEqual(players[1].name, "Lemieux")
         self.assertEqual(players[2].name, "Yzerman")
+        self.assertEqual(players[3].name, "Kurri")
+        self.assertEqual(players[4].name, "Semenko")
+
+        players = self.stats.top(4, SortBy.POINTS)
+        self.assertEqual(players[0].name, "Gretzky")
+        self.assertEqual(players[1].name, "Lemieux")
+        self.assertEqual(players[2].name, "Yzerman")
+        self.assertEqual(players[3].name, "Kurri")
+        self.assertEqual(players[4].name, "Semenko")
+
+    def test_top_goals(self):
+        players = self.stats.top(4, SortBy.GOALS)
+        self.assertEqual(players[0].name, "Lemieux")
+        self.assertEqual(players[1].name, "Yzerman")
+        self.assertEqual(players[2].name, "Kurri")
+        self.assertEqual(players[3].name, "Gretzky")
+        self.assertEqual(players[4].name, "Semenko")
+
+    def test_top_assists(self):
+        players = self.stats.top(4, SortBy.ASSISTS)
+        self.assertEqual(players[0].name, "Gretzky")
+        self.assertEqual(players[1].name, "Yzerman")
+        self.assertEqual(players[2].name, "Lemieux")
         self.assertEqual(players[3].name, "Kurri")
         self.assertEqual(players[4].name, "Semenko")
