f64878efd6f2de78873a2f9ed3386f46af2134df
asnabryg <8mixu8@gmail.com>
2022-11-20 17:36:06 +0200

    3.1

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,7 +1,36 @@
+import requests
 from player import Player
 
+
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2021-22/players"
+    response = requests.get(url).json()
+
+    # print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name'],
+            player_dict['nationality'],
+            player_dict['assists'],
+            player_dict['goals'],
+            player_dict['penalties'],
+            player_dict['team'],
+            player_dict['games'],
+        )
+
+        players.append(player)
+
+    print("Players from FIN")
+    fin_players = [player for player in players if player.nationality == "FIN"]
+    fin_players.sort(
+        key=lambda x: (x.goals + x.assists), reverse=True)
+    for player in fin_players:
+        print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,12 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, nationality, assists, goals, penalties, team, games):
         self.name = name
-    
+        self.nationality = nationality
+        self.assists = assists
+        self.goals = goals
+        self.penalties = penalties
+        self.team = team
+        self.games = games
+
     def __str__(self):
-        return self.name
+        return self.name + " team " + self.team + "  goals " + str(self.goals) + " assists " + str(self.assists)
