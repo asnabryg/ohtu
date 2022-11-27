9084f35e45610f6c4b605a6a595d8ceec186f0d3
asnabryg <8mixu8@gmail.com>
2022-11-27 14:31:49 +0200

    refractored some code

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -5,42 +5,38 @@ from ostos import Ostos
 class Ostoskori:
     def __init__(self):
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
-        self.kaikki_ostokset = {}
+        self._ostokset = {}
 
     def tavaroita_korissa(self):
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2
-        maara = 0
-        for ostos in self.kaikki_ostokset:
-            maara += self.kaikki_ostokset[ostos].lukumaara()
-        return maara
+        return sum([self._ostokset[ostos].lukumaara() for ostos in self._ostokset])
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
-        hinta = 0
-        for ostos in self.kaikki_ostokset:
-            hinta += self.kaikki_ostokset[ostos].hinta()
-        return hinta
+        return sum([self._ostokset[ostos].hinta() for ostos in self._ostokset])
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        if lisattava.nimi() not in self.kaikki_ostokset:
-            self.kaikki_ostokset[lisattava.nimi()] = Ostos(lisattava)
+        nimi = lisattava.nimi()
+        if nimi not in self._ostokset:
+            self._ostokset[nimi] = Ostos(lisattava)
         else:
-            self.kaikki_ostokset[lisattava.nimi()].muuta_lukumaaraa(1)
+            self._ostokset[nimi].muuta_lukumaaraa(1)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
-        self.kaikki_ostokset[poistettava.nimi()].muuta_lukumaaraa(-1)
-        if self.kaikki_ostokset[poistettava.nimi()].lukumaara() == 0:
-            del self.kaikki_ostokset[poistettava.nimi()]
+        nimi = poistettava.nimi()
+        self._ostokset[nimi].muuta_lukumaaraa(-1)
+        if self._ostokset[nimi].lukumaara() == 0:
+            del self._ostokset[nimi]
 
     def tyhjenna(self):
         # tyhjentää ostoskorin
-        self.kaikki_ostokset = {}
+        self._ostokset = {}
 
     def ostokset(self):
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
-        return list(self.kaikki_ostokset.values())
+        return list(self._ostokset.values())
